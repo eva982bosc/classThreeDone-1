da35406f5b6a4d8c77e713a7e85c32116d893c15
Eva Chen <ec2604@nyu.edu>
2021-06-10 21:17:27 -0400

    added content to page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,13 +1,37 @@
-var tabs = document.querySelectorAll("nav a");
+var tabs = document.querySelectorAll('nav a')
+contentPara = document.querySelector('.content')
 
-tabs.forEach((tab) => tab.addEventListener("click", makeActive));
+//tabs.forEach((tab) => tab.addEventListener('click', makeActive))
 
 function makeActive(event) {
-  makeInactive();
-  event.target.classList.add("active");
-  event.preventDefault();
+  if (!event.target.matches('a')) return
+  makeInactive()
+  event.target.classList.add('active')
 }
+//event.preventDefault() - dont need to prevent degault because doesnt have pages that nagivate away. hash routing.
 
 function makeInactive() {
-  tabs.forEach((tab) => tab.classList.remove("active"));
+  tabs.forEach((tab) => tab.classList.remove('active'))
 }
+
+function setContentAccordingToHash() {
+  const type = window.location.hash.substring(1)
+  contentPara.innerHTML = data[type]
+}
+
+function initializePage() {
+  if (!window.location.hash) {
+    window.location.hash = 'cuisines'
+    document.querySelector('[href="#cuisines"]').classList.add('active')
+  } else {
+    document
+      .querySelector(`[href="${window.location.hash}]"`)
+      .classList.add('active')
+  }
+  setContentAccordingToHash()
+}
+
+document.addEventListener('click', makeActive)
+window.addEventListener('hashchange', setContentAccordingToHash)
+
+initializePage()
